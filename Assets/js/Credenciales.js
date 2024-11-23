a608086f0d1e8e71ed9081c6f4fd6103b9a8c9c8
frank-oceann <140687903+frank-oceann@users.noreply.github.com>
2024-11-22 23:55:11 -0300

    Revert "fin"
    This reverts commit b03f46969d42c5b7aa4fb5da18b129561844b2a7.

--- Assets/js/Credenciales.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyD9pEz0pj-aTJ4vEe4KM2RRqONsMrTU8HA",
-    authDomain: "eva3frontend.firebaseapp.com",
-    projectId: "eva3frontend",
-    storageBucket: "eva3frontend.firebasestorage.app",
-    messagingSenderId: "728584472978",
-    appId: "1:728584472978:web:d74a55fe143cd4cf1ad94b",
-    measurementId: "G-8H8P55H1QH"
-  };
